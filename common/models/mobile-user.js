168209e1754963a34be5105a2887ddd80ee4ff17
James Sentell <jamessentell1@gmail.com>
2015-07-20 12:39:12 -0500

    Correcting naming error for weather report model config

--- common/models/mobile-user.js
+++ common/models/mobile-user.js
@@ -8,7 +8,7 @@ module.exports = function(MobileUser) {
 	MobileUser.disableRemoteMethod('__get__position', false);
 	MobileUser.disableRemoteMethod('__updateById__position', false);	
 	
-	// Hide internal "weather_report" relationship API endpoints
+	// Hide internal "weatherreport" relationship API endpoints
 	MobileUser.disableRemoteMethod('__count__weatherreport', false);
 	MobileUser.disableRemoteMethod('__create__weatherreport', false);
 	MobileUser.disableRemoteMethod('__delete__weatherreport', false);
