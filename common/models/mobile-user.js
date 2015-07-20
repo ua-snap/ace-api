adfc0f76c2c5b3f4d9e956e303b71d3baa20a831
James Sentell <jamessentell1@gmail.com>
2015-07-20 10:30:51 -0500

    Correcting hasManyThrough relationship between Group and Position.

--- common/models/mobile-user.js
+++ common/models/mobile-user.js
@@ -1,3 +1,10 @@
 module.exports = function(MobileUser) {
-
+	// Hide internal "position" relationship API endpoints
+	MobileUser.disableRemoteMethod('__count__position', false);
+	MobileUser.disableRemoteMethod('__create__position', false);
+	MobileUser.disableRemoteMethod('__delete__position', false);
+	MobileUser.disableRemoteMethod('__destroyById__position', false);
+	MobileUser.disableRemoteMethod('__findById__position', false);
+	MobileUser.disableRemoteMethod('__get__position', false);
+	MobileUser.disableRemoteMethod('__updateById__position', false);	
 };
