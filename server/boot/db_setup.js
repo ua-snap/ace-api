dd9cc3f5ba8894be6c12636b338a597043a33ce7
jamessentell <sentelljg@SentellJG-Mac.local>
2015-07-23 14:59:05 -0500

    correcting initial data entry

--- server/boot/db_setup.js
+++ server/boot/db_setup.js
@@ -4,26 +4,30 @@ module.exports = function(app) {
 	app.dataSources.postgresql_heroku.automigrate('group', function(err) {
 		if (err) throw err;
 		
-		app.models.group.create([{name: "IonicViewUsers"}]);
-		app.models.group.create([{name: "DummyUserAccounts"}]);
-	});
+		// Mobile Users
+		app.dataSources.postgresql_heroku.automigrate('mobile_user', function(err) {
+			if(err) throw err;
+			
+			app.models.group.create([{name: "TestUsers"}], function(err, group) {
+				app.models.mobile_user.create([
+					{username: "jsentell", email: "james.g.sentell@saic.com", password:"password", groupId: group.id},
+					{username: "jpowell", email: "jonathan.d.powell@saic.com", password:"password", groupId: group.id},
+					{username: "pmeyer", email: "paul.meyer@nasa.gov", password:"password", groupId: 1},
+					{username: "hhelton", email: "heather.l.helton3.civ@mail.mil", password:"password", groupId: group.id},
+					{username: "sspehn", email: "stephen.l.spehn.civ@mail.mil", password:"password", groupId: group.id},
+					{username: "jpanter", email: "james.e.panter@saic.com", password:"password", groupId: group.id}
+				]);
+			});
+				
+			app.models.group.create([{name: "DummyUserAccounts"}]);	
+				app.models.mobile_user.create([
+					{username: "appuser", email: "test@test.com", password:"password", groupId: 2},
+					{username: "appuser2", email: "test2@test.com", password:"password", groupId: 2},
+					{username: "appuser3", email: "test3@test.com", password:"password", groupId: 2}			
+				]);				
+			});			
+		});
 	
-	// Mobile Users
-	app.dataSources.postgresql_heroku.automigrate('mobile_user', function(err) {
-		if (err) throw err;
-		
-		app.models.mobile_user.create([
-			{username: "appuser", email: "test@test.com", password:"password", groupId: 2},
-			{username: "appuser2", email: "test2@test.com", password:"password", groupId: 2},
-			{username: "appuser3", email: "test3@test.com", password:"password", groupId: 2},
-			{username: "jsentell", email: "james.g.sentell@saic.com", password:"password", groupId: 1},
-			{username: "jpowell", email: "jonathan.d.powell@saic.com", password:"password", groupId: 1},
-			{username: "pmeyer", email: "paul.meyer@nasa.gov", password:"password", groupId: 1},
-			{username: "hhelton", email: "heather.l.helton3.civ@mail.mil", password:"password", groupId: 1},
-			{username: "sspehn", email: "stephen.l.spehn.civ@mail.mil", password:"password", groupId: 1},
-			{username: "jpanter", email: "james.e.panter@saic.com", password:"password", groupId: 1},
-		]);
-	});
 	
 	// Position
 	app.dataSources.postgresql_heroku.automigrate('position', function(err) {
