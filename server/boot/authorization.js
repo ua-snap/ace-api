f083fc7d78e5af4695cb073ebfc2b1acf6d6573d
jamessentell <sentelljg@SentellJG-Mac.local>
2015-08-04 12:08:51 -0500

    Correcting error in startup script

--- server/boot/authorization.js
+++ server/boot/authorization.js
@@ -1,6 +1,6 @@
-module.exports = function (app, loopback) {
+module.exports = function (app, server) {
   // setup additional authorization keys
-  app.use(loopback.token({
+  app.use(server.loopback.token({
     headers: ['authorization', 'X-Access-Token', 'x-access-token']
   }));
 };
