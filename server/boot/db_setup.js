1697d3861fc0a6f663738513467ba0554d2c2e2f
jamessentell <sentelljg@SentellJG-Mac.local>
2015-08-05 17:02:05 -0500

    Changing boot script to avoid clearing all previous data on reboots.

--- server/boot/db_setup.js
+++ server/boot/db_setup.js
@@ -1,72 +1,79 @@
 // Automigrates all data models
 module.exports = function(app) {
 	
-	app.dataSources.postgresql_heroku.automigrate(['User', 'AccessToken', 'RoleMapping', 'Role', 'ACL'], function(err) {
+	app.dataSources.postgresql_heroku.isActual(['User', 'AccessToken', 'RoleMapping', 'Role', 'ACL', 'Checkpoint', 'Change', 'group', 'group-change', 'mobile_user', 'mobile_user-change', 'position', 'position-change', 'weatherreport', 'weatherreport-change'], function(err, actual) {
 		if(err) throw err;
 		
-		app.dataSources.postgresql_heroku.automigrate(['Checkpoint', 'Change'], function(err) {
-			if(err) throw err;
-			
-			// 	Groups
-			app.dataSources.postgresql_heroku.automigrate('group', function(err) {
-				if (err) throw err;
+		if(!actual)
+		{
+			app.dataSources.postgresql_heroku.automigrate(['User', 'AccessToken', 'RoleMapping', 'Role', 'ACL'], function(err) {
+				if(err) throw err;
 				
-				// Auto-migrate sync for group (group-change)
-				app.dataSources.postgresql_heroku.automigrate('group-change', function(err) {
+				app.dataSources.postgresql_heroku.automigrate(['Checkpoint', 'Change'], function(err) {
 					if(err) throw err;
 					
-					// Mobile Users
-					app.dataSources.postgresql_heroku.automigrate('mobile_user', function(err) {
-						if(err) throw err;
+					// 	Groups
+					app.dataSources.postgresql_heroku.automigrate('group', function(err) {
+						if (err) throw err;
 						
-						// Mobile Users change
-						app.dataSources.postgresql_heroku.automigrate('mobile_user-change', function(err) {
+						// Auto-migrate sync for group (group-change)
+						app.dataSources.postgresql_heroku.automigrate('group-change', function(err) {
 							if(err) throw err;
 							
-							// Now all users and groups can be added
-							app.models.group.create([{name: "TestUsers"}], function(err, group) {
-								app.models.mobile_user.create([
-									{username: "jsentell", email: "james.g.sentell@saic.com", password:"password", groupId: group[0].id},
-									{username: "jpowell", email: "jonathan.d.powell@saic.com", password:"password", groupId: group[0].id},
-									{username: "pmeyer", email: "paul.meyer@nasa.gov", password:"password", groupId: group[0].id},
-									{username: "hhelton", email: "heather.l.helton3.civ@mail.mil", password:"password", groupId: group[0].id},
-									{username: "sspehn", email: "stephen.l.spehn.civ@mail.mil", password:"password", groupId: group[0].id},
-									{username: "jpanter", email: "james.e.panter@saic.com", password:"password", groupId: group[0].id}
-								]);
-							});
+							// Mobile Users
+							app.dataSources.postgresql_heroku.automigrate('mobile_user', function(err) {
+								if(err) throw err;
 								
-							app.models.group.create([{name: "DummyUserAccounts"}], function(err, group) {
-								app.models.mobile_user.create([
-									{username: "appuser", email: "test@test.com", password:"password", groupId: group[0].id},
-									{username: "appuser2", email: "test2@test.com", password:"password", groupId: group[0].id},
-									{username: "appuser3", email: "test3@test.com", password:"password", groupId: group[0].id}			
-								]);
+								// Mobile Users change
+								app.dataSources.postgresql_heroku.automigrate('mobile_user-change', function(err) {
+									if(err) throw err;
+									
+									// Now all users and groups can be added
+									app.models.group.create([{name: "TestUsers"}], function(err, group) {
+										app.models.mobile_user.create([
+											{username: "jsentell", email: "james.g.sentell@saic.com", password:"password", groupId: group[0].id},
+											{username: "jpowell", email: "jonathan.d.powell@saic.com", password:"password", groupId: group[0].id},
+											{username: "pmeyer", email: "paul.meyer@nasa.gov", password:"password", groupId: group[0].id},
+											{username: "hhelton", email: "heather.l.helton3.civ@mail.mil", password:"password", groupId: group[0].id},
+											{username: "sspehn", email: "stephen.l.spehn.civ@mail.mil", password:"password", groupId: group[0].id},
+											{username: "jpanter", email: "james.e.panter@saic.com", password:"password", groupId: group[0].id}
+										]);
+									});
+										
+									app.models.group.create([{name: "DummyUserAccounts"}], function(err, group) {
+										app.models.mobile_user.create([
+											{username: "appuser", email: "test@test.com", password:"password", groupId: group[0].id},
+											{username: "appuser2", email: "test2@test.com", password:"password", groupId: group[0].id},
+											{username: "appuser3", email: "test3@test.com", password:"password", groupId: group[0].id}			
+										]);
+									});
+								});
 							});
 						});
 					});
+					
+					
+					// Position
+					app.dataSources.postgresql_heroku.automigrate('position', function(err) {
+						if (err) throw err;
+						
+						// Migrate sync for mobile_user
+						app.dataSources.postgresql_heroku.automigrate('position-change', function(err) {
+							if(err) throw err;
+						});
+					});
+					
+					// Weather Report
+					app.dataSources.postgresql_heroku.automigrate('weatherreport', function(err) {
+						if (err) throw err;
+						
+						// Migrate sync for mobile_user
+						app.dataSources.postgresql_heroku.automigrate('weatherreport-change', function(err) {
+							if(err) throw err;
+						});
+					});
 				});
-			});
-			
-			
-			// Position
-			app.dataSources.postgresql_heroku.automigrate('position', function(err) {
-				if (err) throw err;
-				
-				// Migrate sync for mobile_user
-				app.dataSources.postgresql_heroku.automigrate('position-change', function(err) {
-					if(err) throw err;
-				});
-			});
-			
-			// Weather Report
-			app.dataSources.postgresql_heroku.automigrate('weatherreport', function(err) {
-				if (err) throw err;
-				
-				// Migrate sync for mobile_user
-				app.dataSources.postgresql_heroku.automigrate('weatherreport-change', function(err) {
-					if(err) throw err;
-				});
-			});
-		});
-	});	
+			});	
+		}
+	});
 };
