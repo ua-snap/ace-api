400e9bfca9bd05d18211f4a1632c2097c87a5b6d
Craig Stephenson <cstephen@iarc.uaf.edu>
2016-06-02 18:26:01 -0800

    Added new GET endpoint that joins WeatherReports with their related Positions.

--- common/models/weather-report.js
+++ common/models/weather-report.js
@@ -2,4 +2,16 @@ module.exports = function(Weatherreport) {
 	Weatherreport.handleChangeError = function(err) {
 		console.warn("Cannot update change records for WeatherReport: ", err);	
 	};
+
+	Weatherreport.withPositions = function(WeatherreportId, cb) {
+		Weatherreport.find({include: 'Position'}, function(err, results) {
+			cb(null, results);
+		}
+	}
+
+	Weatherreport.remoteMethod('withPositions', {
+	    http: {path: '/with-positions', verb: 'get'},
+	    accepts: {arg: 'id', type: 'string', http: { source: 'query' } },
+	    returns: {arg: 'WeatherReports', type: 'string'}
+	});
 };
