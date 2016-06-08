47a1c2797accc2c96c151e65a5e03e4e92be795c
Craig Stephenson <cstephen@iarc.uaf.edu>
2016-06-07 18:21:36 -0800

    Rename custom API endpoint to "aaep", remove arguments, and filter on new aaep boolean field.

--- common/models/weather-report.js
+++ common/models/weather-report.js
@@ -3,15 +3,19 @@ module.exports = function(Weatherreport) {
 		console.warn("Cannot update change records for WeatherReport: ", err);	
 	};
 
-	Weatherreport.withPositions = function(WeatherreportId, cb) {
-		Weatherreport.find({include: 'Position'}, function(err, results) {
+	Weatherreport.withPositions = function(cb) {
+		Weatherreport.find({
+			where: {'aaep': true},
+			include: {
+				relation: 'Position'
+			}
+		}, function(err, results) {
 			cb(null, results);
-		}
-	}
+		});
+	};
 
 	Weatherreport.remoteMethod('withPositions', {
-	    http: {path: '/with-positions', verb: 'get'},
-	    accepts: {arg: 'id', type: 'string', http: { source: 'query' } },
-	    returns: {arg: 'WeatherReports', type: 'string'}
+		http: {path: '/aaep', verb: 'get'},
+		returns: {arg: 'WeatherReports', type: 'string'}
 	});
 };
